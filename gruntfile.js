a2d4474d1088483fe3e81af92f8a6c3e11a4c8d9
Omar Morales <omoral02@live.com>
2018-02-27 00:49:54 +0000

    Update script files

--- gruntfile.js
+++ gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
         },
         watch: {
           jsScripts: {
-            files: ['src/js/*.js','src/gruntfile.js'],
+            files: ['src/js/*.js','src/*.js'],
             tasks: ['jshint:beforeconcat', 'concat', 'jshint:afterconcat']
           },
           manifest: {
