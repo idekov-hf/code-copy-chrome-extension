6f1a2a2d1b232d1302b84a873c8fc15058dc2921
Iavor Dekov <ivdekov@colby.edu>
2018-02-27 21:47:02 +0000

    Get copy button to work; position it correctly

--- src/js/codeblock.js
+++ src/js/codeblock.js
@@ -1,45 +1,80 @@
 var CodeBlock = (function() {
-    
+
     var copyButtonContainer;
-    
-    function displayCopyButton(codeContainer) {
-        codeContainer.appendChild(copyButtonContainer);
+    var preElements;
+    var preIndex;
+
+    function selectText(element) {
+        var text = element;
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
-    
-    function hideCopyButton(codeContainer) {
-        codeContainer.removeChild(copyButtonContainer);
-    }
-    
+
     function createCopyButton() {
         var buttonContainer = document.createElement('div');
         var button = document.createElement('button');
         buttonContainer.classList.add('copy-button-container');
         button.textContent = 'Copy Code';
         buttonContainer.appendChild(button);
-        
+
+        button.addEventListener('click', function() {
+            var preElement = preElements[preIndex];
+            var codeElement = preElement.children[0];
+            selectText(codeElement);
+            document.execCommand("copy");
+            var selection = window.getSelection();
+            selection.removeAllRanges();
+        });
+
         return buttonContainer;
     }
-    
-    function setupListeners(codeElements) {
-        codeElements.forEach(function(element) {
-            var parentNode = element.parentNode;
-            parentNode.style.position = 'relative';
-			parentNode.addEventListener('mouseenter', function() {
-			    displayCopyButton(parentNode);
-			});
-			parentNode.addEventListener('mouseleave', function() {
-			    hideCopyButton(parentNode);
-			});
-		});
+
+    function createNewContainer(element) {
+        var parentNode = element.parentNode;
+        var newContainer = document.createElement('div');
+        newContainer.classList.add('new-container');
+        newContainer.style.position = 'relative';
+        newContainer.appendChild(element);
+        parentNode.appendChild(newContainer);
+
+        return newContainer;
     }
-    
+
+    function displayCopyButton(codeContainer) {
+        codeContainer.appendChild(copyButtonContainer);
+    }
+
+    function hideCopyButton(codeContainer) {
+        codeContainer.removeChild(copyButtonContainer);
+    }
+
+    function setupListeners(newContainer, index) {
+        newContainer.addEventListener('mouseenter', function() {
+            displayCopyButton(newContainer);
+            preIndex = index;
+        });
+        newContainer.addEventListener('mouseleave', function() {
+            hideCopyButton(newContainer);
+        });
+    }
+
+    function handlePreElements(preElements) {
+        preElements.forEach(function(element, index) {
+            var newContainer = createNewContainer(element);
+
+            setupListeners(newContainer, index);
+        });
+    }
+
     function init() {
         copyButtonContainer = createCopyButton();
-        var codeElements = document.querySelectorAll('pre code');
-        
-        setupListeners(codeElements);
+        preElements = document.querySelectorAll('.post-text pre');
+        handlePreElements(preElements);
     }
-    
+
     return {
         init: init
     };
